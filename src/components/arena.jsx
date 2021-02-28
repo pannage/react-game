a34bf7285a7116161f6025b02b89156de74d33d4
Tatsiana <pannage@gmail.com>
2021-02-28 20:37:03 +0300

    init: initial commit

---
+++ src/components/arena.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import '../styles/styles.css';
+
+export class Arena2 extends Component {
+    render() {
+        return (
+            <div className="arena2">
+                <div className={`${this.props.pick} arena`}></div>
+                <div className="result">
+                    <h3 className="result-message">{this.props.message}</h3>
+                    <button className="button replay icon-btn" onClick = {() => this.props.replay()}>replay</button>
+                </div>
+                <div className={`${this.props.comp1} arena`}></div>
+            </div>
+        );
+    }
+}
+
+export class Arena3 extends Component {
+    render() {
+        return (
+            <div className="arena3">
+                <div className={`${this.props.pick}`}></div>
+                <div className="simple-txt"></div>
+                <div className={`${this.props.comp1}`}></div>
+            </div>
+        );
+    }
+}
