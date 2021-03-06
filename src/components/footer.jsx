9255a758eb40541ce9db334b94bd6b407727b2f7
Tatsiana <pannage@gmail.com>
2021-03-06 23:50:38 +0300

    feat: add link to pull request

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -13,7 +13,7 @@ export default class Footer extends Component {
                         <img src="https://www.flaticon.com/svg/static/icons/svg/2317/2317981.svg" className="social-list__item-img" alt="github-logo" title="github account pannage"/>
                     </a>
                     <a href="https://rs.school/js/" className="social-list__item" alt="rsschool">
-                        <img src="../../assets/rs-school.png" className="social-list__item-img rsschool-logo" alt="rsschool-logo"/>
+                        <div className="logo"></div>
                     </a>
                 </div>
             </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,7 +6,7 @@ export default class Header extends Component {
     render() {
         return (
             <div className="header">
-
+                <a href='https://github.com/pannage/react-game/pull/1' className="link">ссылка на Pull Request</a>
             </div>
         );
     }
